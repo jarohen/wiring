cd3de0334b0e5b4ce28f5c786cf63833f24054b5
James Henderson <james@jarohen.me.uk>
2017-05-12 14:56:27 +0100

    Applying switches in the config

--- src/wiring/core.clj
+++ src/wiring/core.clj
@@ -1,18 +1,28 @@
 (ns wiring.core)
 
+(defn noop [])
+
 (defrecord Component [value stop!])
 
 (defn ->component
-  ([value] (->component value (fn [])))
+  ([value] (->component value noop))
   ([value stop!] (->Component value stop!)))
 
-(defn start-system [config]
+(defn apply-switches [config {:keys [switches]}]
+  (apply merge (dissoc config :wiring/switches)
+         (for [switch switches]
+           (get-in config [:wiring/switches switch]))))
+
+(defn start-system [config {:keys [switches]}]
   (reduce (fn [system [k {component-fn :wiring/component, :as component-config}]]
-            (let [started-component (component-fn (dissoc component-config :wiring/component))]
+            (let [started-component (component-fn (-> component-config
+                                                      (apply-switches {:switches switches})
+                                                      (dissoc :wiring/component :wiring/switches)))]
               (-> system
                   (assoc-in [:components k] (cond-> started-component
                                               (not (instance? Component started-component)) ->component))
                   (update :component-order conj k))))
+
           {:component-order []}
           config))
 
--- test/wiring/core_test.clj
+++ test/wiring/core_test.clj
@@ -2,22 +2,39 @@
   (:require [wiring.core :as sut]
             [clojure.test :as t]))
 
+(t/deftest applies-switches
+  (t/is (= (sut/apply-switches {:wiring/switches {:the-switch {:switched? true
+                                                               :overruled? false}
+                                                  :other-switch {:overruled? true}
+                                                  :not-this-one {:uh-oh? true}}
+
+                                :config-key :value
+                                :switched? false}
+
+                               {:switches [:the-switch :other-switch]})
+           {:config-key :value
+            :switched? true
+            :overruled? true})))
+
 (t/deftest one-component-system
   (let [!log (atom [])
         component-config {:config-key :value}
-        system-map {:component (merge {:wiring/component (fn [config]
-                                                           (swap! !log conj [:start-component config])
-                                                           (sut/->component :started-component
-                                                                            (fn []
-                                                                              (swap! !log conj :stop-component))))}
-                                      component-config)}
+        system-map {:component {:wiring/component (fn [config]
+                                                    (swap! !log conj [:start-component config])
+                                                    (sut/->component :started-component
+                                                                     (fn []
+                                                                       (swap! !log conj :stop-component))))
 
-        started-system (sut/start-system system-map)]
+                                :wiring/switches {:the-switch {:switched? true}
+                                                  :not-this-one {:uh-oh? true}}
 
-    (t/is (= @!log [[:start-component component-config]]))
+                                :config-key :value
+                                :switched? false}}
+
+        started-system (sut/start-system system-map {:switches [:the-switch]})]
+
+    (t/is (= @!log [[:start-component {:config-key :value, :switched? true}]]))
 
     (sut/stop-system started-system)
 
-    (t/is (= @!log [[:start-component component-config] :stop-component]))
-
-    ))
+    (t/is (= @!log [[:start-component {:config-key :value, :switched? true}] :stop-component]))))
